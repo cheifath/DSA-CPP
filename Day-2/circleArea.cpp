adbb53c3a754a18eef4bf418bea7d5a517027391
Cheifath <cheifathmohammed@gmail.com>
2024-12-10 18:29:19 +0530

    Add program3

---
+++ Day-2/circleArea.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float pie=3.14159,radius=0;
+    cout<<"Enter the radius of the circle:";
+    cin>>radius;
+    cout<<"Area of the circle : "<<pie*radius*radius;
+    return 0;
+}
