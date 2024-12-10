e5fa1f4434126f8f8c08363ec1a1808f06e76114
Cheifath <cheifathmohammed@gmail.com>
2024-12-10 18:18:12 +0530

    Add program1

---
+++ Day-2/arithemeticOperator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cout<<"Enter two number:";
+    cin>>n1>>n2;
+    cout<<"Addition="<<n1+n2<<endl;
+    cout<<"Substraction="<<n1-n2<<endl;
+    cout<<"Multiplication="<<n1*n2<<endl;
+    cout<<"Division="<<n1/n2<<endl;
+    cout<<"Modulus="<<n1%n2<<endl;
+    return 0;
+}
