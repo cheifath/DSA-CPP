8b9a028878e11fc8c493e625320e20c57e0c5f0f
Cheifath <cheifathmohammed@gmail.com>
2024-12-10 18:25:24 +0530

    Add program2

---
+++ Day-2/datatype.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int integer;
+    char character;
+    float floating;
+    cout<<"Enter an integer:";
+    cin>>integer;
+    cout<<"Enter a character:";
+    cin>>character;
+    cout<<"Enter a floating value:";
+    cin>>floating;
+    cout<<"Int="<<integer<<"\nChar="<<character<<"\nFloat="<<floating;
+    return 0;
+    
+}
